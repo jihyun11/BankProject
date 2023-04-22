13f3ee1158278be18437563d6ce34812960f943c
주현태(Hyeontae Ju)/기프티콘개발팀/11ST <juhyeontae@11sk.com>
2023-04-22 10:55:42 +0900

    feat(#1) : 클래스 추가(기능은 todo로 남겨둠)
    클래스 추가(기능은 todo로 남겨둠)
    
    - 고객정보
    - 대출정보
    - 주택정보

---
+++ src/main/java/com/example/bank/houseloan/Customer.java
@@ -0,0 +1,27 @@
+package com.example.bank.houseloan;
+
+public class Customer {
+
+    private String 주민번호;
+    private String 성명;
+
+    public boolean 고객등록(String 주민번호, String 성명) {
+        // todo
+        return false;
+    }
+
+    public boolean 고객수정(String 주민번호, String 성명) {
+        // todo
+        return false;
+    }
+
+    public boolean 고객삭제(String 주민번호) {
+        // todo
+        return false;
+    }
+
+    public boolean 고객조회(String 주민번호) {
+        // todo
+        return false;
+    }
+}
---
+++ src/main/java/com/example/bank/houseloan/Loan.java
@@ -0,0 +1,12 @@
+package com.example.bank.houseloan;
+
+public class Loan {
+    private String 대출id;
+    private String 주민번호;
+    private String 주소;
+    private double 대출금액;
+
+    // todo 누락된 property 추가
+
+    // todo 메소드 추가
+}
